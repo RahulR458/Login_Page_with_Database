27739a407d5e1bb37b86169ded86fc9b83be7b98
unknown <rahulramesh458@gmail.com>
2023-08-14 10:02:23 +0530

    second commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require(`morgan`);
 const bodyparser = require("body-parser");
 const path = require("path")
 
+const connectDB = require('./server/database/connection')
+
 const app = express();
 
 dotenv.config({path:`config.env`})
@@ -16,7 +18,7 @@ app.use(morgan(`tiny`));
 app.use(bodyparser.urlencoded({extended:true}))
 
 //set view engine
-app.set("view engine","ejs")
+app.set("view engine","ejs") 
 // app.set("views",path.resolve(__dirname,"views/ejs"))
 
 //load assets
@@ -24,10 +26,8 @@ app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
 app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
 app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
-
-app.get('/',(req,res)=>{
-    res.render("index");
-})
+//loard router
+app.use('/',require('./server/routes/router'))
 
 app.listen(3000,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
